7c8419cc5be158d11af2783340300262eb442bfc
Ryoya Tamura <lollipop@simochee.net>
2023-11-08 18:46:14 +0900

    🐛 fix js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const d = new Date();
 
-console.log(new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'full', timeZone: 'Asia/Tokyo' }));
+console.log(
+  new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+    timeStyle: "full",
+    timeZone: "Asia/Tokyo",
+  }).format(d)
+);
